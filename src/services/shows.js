be12e5e92b9b311e2617e115c5105628aa0d0cf0
harikavurranki@gmail.com <harikavurranki@gmail.com>
2020-10-19 12:17:03 +0530

    used base url and chnaged to string literals

--- src/services/shows.js
+++ src/services/shows.js
@@ -1,32 +1,32 @@
 import CommonService from './index.js'
-
+const baseURL = 'http://api.tvmaze.com'
 export default {
   getShowDetails () {
-    const response = CommonService.getData('http://api.tvmaze.com/shows').then((res) => {
+    const response = CommonService.getData(`${baseURL}/shows`).then((res) => {
       return res
     })
     return response
   },
   getShowSeasons (showId) {
-    const response = CommonService.getData('http://api.tvmaze.com/shows/' + showId + '/seasons').then((res) => {
+    const response = CommonService.getData(`${baseURL}/shows/${showId}/seasons`).then((res) => {
       return res
     })
     return response
   },
-  getEpisodesDataBySeasonId (id) {
-    const response = CommonService.getData('http://api.tvmaze.com/seasons/' + id + '/episodes').then((res) => {
+  getEpisodesDataBySeasonId (seasonId) {
+    const response = CommonService.getData(`${baseURL}/seasons/${seasonId}/episodes`).then((res) => {
       return res
     })
     return response
   },
   getSearchResultsByQuery (query) {
-    const response = CommonService.getData('http://api.tvmaze.com/search/shows?q=' + query).then((res) => {
+    const response = CommonService.getData(`${baseURL}/search/shows?q=${query}`).then((res) => {
       return res
     })
     return response
   },
   getShowCastDetails (showId) {
-    const response = CommonService.getData('http://api.tvmaze.com/shows/' + showId + '/cast').then((res) => {
+    const response = CommonService.getData(`${baseURL}/shows/${showId}/cast`).then((res) => {
       return res
     })
     return response
